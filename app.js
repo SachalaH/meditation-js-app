b64accc4828cdbd163600f6c7a2bdfafc833ef6e
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-09-28 23:52:06 +0530

    Added progress bar, minutes button & change song

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = () => {
   const play = document.querySelector(".play");
   const outline = document.querySelector(".moving-outline circle");
   const video = document.querySelector(".video-container video");
+  const timeSelect = document.querySelectorAll(".time-select button");
 
   //  Sounds
   const sounds = document.querySelectorAll(".sound-picker button");
@@ -11,14 +12,36 @@ const app = () => {
   // Get the length of outline
   const outlineLength = outline.getTotalLength();
   // Duration
-  let fakeDuration = 600;
+  let fakeDuration = 180;
+  timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+    fakeDuration % 60
+  )}`;
 
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  // select different song
+  sounds.forEach((sound) => {
+    sound.addEventListener("click", function () {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      play.src = "./svg/play.svg";
+    });
+  });
+
   //   play sound
   play.addEventListener("click", () => {
     checkPlaying(song);
+  });
+
+  // select time
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
   });
 
   //   lets create a function to play pause song and display buttons acc.
@@ -31,6 +54,27 @@ const app = () => {
       song.pause();
       video.pause();
       play.src = "./svg/play.svg";
+    }
+  };
+  // Animate the circle
+  song.ontimeupdate = () => {
+    let current_time = song.currentTime;
+    let elapsed = fakeDuration - current_time;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    // Animate the circle
+    let progress =
+      outlineLength - (current_time / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    // Animate display
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+    // Pause video and audio on completion
+    if (current_time >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
     }
   };
 };
